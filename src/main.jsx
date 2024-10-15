f74ee79ed84d666933296d3df3e6701db6369db8
Roman Oryol <roman.oryol83@gmail.com>
2024-10-15 14:24:13 +0300

    unnecessary imports removed

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './components/app/App';
-import { ThemeProvider } from 'styled-components';
-import { defaultTheme } from './themes/default';
+import App from './App';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ThemeProvider theme={defaultTheme}>
-      <App />
-    </ThemeProvider>
+    <App />
   </StrictMode>
 );
